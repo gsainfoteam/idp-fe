87a6f9588acab12dcc0ae341f58b1d2616c3f29c
2paperstar <lee@paperst.ar>
2023-07-08 15:17:28 +0900

    fix: remove outline from button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,7 +4,6 @@ const Button = styled.button`
   color: var(--color-white);
   background-color: var(--color-primary);
   padding: 0.625rem;
-  outline: none;
   border: none;
   border-radius: 5px;
   min-height: 50px;
