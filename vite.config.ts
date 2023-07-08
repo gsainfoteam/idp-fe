7ed61e5390f881935d78d9b025daee254545c15a
2paperstar <lee@paperst.ar>
2023-07-08 13:29:13 +0900

    feat: init with template

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
+import svgr from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    outDir: "build",
+  },
+  server: {
+    open: true,
+    port: 3000,
+    proxy: {
+      "/local": {
+        target: "https://api.stg.idp.gistory.me",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/local/, ""),
+      },
+    },
+  },
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: "eslint src/",
+        dev: {
+          logLevel: ["warning"],
+        },
+      },
+      enableBuild: false,
+    }),
+    svgr(),
+  ],
+});
