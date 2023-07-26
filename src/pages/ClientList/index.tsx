12d552050b3a91523609cc76c436feb465abdfca
2paperstar <lee@paperst.ar>
2023-07-26 17:41:08 +0900

    feat: show client list with clident card component

--- src/pages/ClientList/index.tsx
+++ src/pages/ClientList/index.tsx
@@ -1,8 +1,35 @@
+import { Link } from "react-router-dom";
 import { useClients } from "src/api/client";
+
+const ClientCard = ({
+  uuid,
+  name,
+  id,
+}: {
+  uuid: string;
+  name: string;
+  id: string;
+}) => (
+  <div>
+    <Link to={`/clients/${uuid}`}>{name}</Link>
+    <div>id: {id}</div>
+  </div>
+);
 
 const ClientListPage = () => {
   const { data: clients } = useClients();
-  return <>{JSON.stringify(clients)}</>;
+  return (
+    <div>
+      <ul>
+        {clients?.map((client) => (
+          <li key={client.uuid}>
+            <ClientCard {...client} />
+          </li>
+        ))}
+      </ul>
+      <Link to="/clients/new">create new client</Link>
+    </div>
+  );
 };
 
 export default ClientListPage;
