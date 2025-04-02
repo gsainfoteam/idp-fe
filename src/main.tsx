333f429c0b7c584674be82e20f47ff4d9c2ba9e7
Cyan <hoony6134@gmail.com>
2025-04-02 15:02:48 +0900

    feat: setup i18n

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './app.tsx';
 
+import './locales/i18n';
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
