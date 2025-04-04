faa9942f42f5def94f2c325edf553e3cfbe92171
Hyunseung Ryu <rhseungg@gmail.com>
2025-04-04 21:53:03 +0900

    fix: remove unused state

--- src/features/test/frames/component-test-frame.tsx
+++ src/features/test/frames/component-test-frame.tsx
@@ -1,10 +1,6 @@
-import { useState } from 'react';
-
 import { BackButton, Button, Checkbox, Input } from '@/features/core';
 
 export function ComponentTestFrame() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="">
       <h1 className="text-title-1 mt-10 ml-10">Typography</h1>
