e185c7ff90487e53a4c024051a064313c10de970
Cyan <hoony6134@gmail.com>
2025-04-06 01:11:30 +0900

    fix: i18n typescript (run npm run interface after modifying translation.json)

--- src/@types/i18next.d.ts
+++ src/@types/i18next.d.ts
@@ -1,13 +1,8 @@
-import 'i18next';
-import ns1 from 'locales/en/translation.json';
-import ns2 from 'locales/en/translation.json';
+import Resources from './resources';
 
 declare module 'i18next' {
   interface CustomTypeOptions {
-    defaultNS: 'ns1';
-    resources: {
-      ns1: typeof ns1;
-      ns2: typeof ns2;
-    };
+    defaultNS: 'translation';
+    resources: Resources;
   }
 }
