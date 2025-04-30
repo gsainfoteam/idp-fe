18dcdc8e00bddf0c812ceb36e040c340280d5268
2paperstar <lee@paperst.ar>
2025-04-30 13:21:50 +0900

    feat: get user hook

--- src/features/auth/hooks/use-auth.ts
+++ src/features/auth/hooks/use-auth.ts
@@ -1,1 +1,24 @@
-export const useAuth = () => {};
+import { useQuery } from '@tanstack/react-query';
+import { useMemo } from 'react';
+
+import { getUser } from '../services/get-user';
+
+import { useToken } from './use-token';
+
+export const useAuth = () => {
+  const { token } = useToken();
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['user'],
+    queryFn: getUser,
+    enabled: !!token,
+  });
+
+  const user = useMemo(() => {
+    if (!token) return null;
+    if (isLoading) return undefined;
+    if (error) return null;
+    return data;
+  }, [token, isLoading, error, data]);
+
+  return { user };
+};
