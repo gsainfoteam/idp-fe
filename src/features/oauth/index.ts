c19a112cba3ef2215b8a9983acf17963540117d2
Hyunseung Ryu <rhseungg@gmail.com>
2025-05-01 13:53:17 +0900

    fix: wrong export

--- src/features/oauth/index.ts
+++ src/features/oauth/index.ts
@@ -2,6 +2,3 @@ export * from './components/authorize-form';
 export * from './frames/authorize-frame';
 export * from './hooks/use-authorize-form';
 export * from './hooks/use-client';
-export * from '../../data/get-client-public';
-export * from '../../data/get-user-consent';
-export * from '../../data/post-oauth-consent';
