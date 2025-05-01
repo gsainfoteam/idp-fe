ee077173751cf310cea8a5941b187af574beea62
2paperstar <lee@paperst.ar>
2025-05-02 07:50:54 +0900

    feat: useAuthorize

--- src/features/oauth/frames/authorize-frame.tsx
+++ src/features/oauth/frames/authorize-frame.tsx
@@ -1,82 +1,71 @@
 import { useLoaderData, useSearch } from '@tanstack/react-router';
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback } from 'react';
 import { FormProvider } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 
 import { AuthorizeForm } from '../components/authorize-form';
+import { useAuthorize } from '../hooks/use-authorize';
 import { useAuthorizeForm } from '../hooks/use-authorize-form';
 import { useClient } from '../hooks/use-client';
 
-import { getUserConsent } from '@/data/get-user-consent';
+import { components } from '@/@types/api-schema';
 import { LoadingOverlay } from '@/features/core';
 
-export function AuthorizeFrame() {
+const Inner = ({
+  client,
+}: {
+  client: components['schemas']['ClientPublicResDto'];
+}) => {
   const { form, onSubmit } = useAuthorizeForm();
   const { t } = useTranslation();
-  const { clientId, scopes } = useLoaderData({
-    from: '/_auth-required/authorize',
-  });
-  const { client } = useClient(clientId);
-  const { prompt, ...search } = useSearch({
-    from: '/_auth-required/authorize',
-  });
 
-  const [isLoading, setLoading] = useState(true);
+  const { prompt: _, ...search } = useSearch({
+    from: '/_auth-required/authorize',
+  });
 
   const authorize = useCallback(() => {
     window.location.href = `${import.meta.env.VITE_API_URL}/oauth/authorize?${new URLSearchParams(search).toString()}`;
   }, [search]);
 
-  useEffect(() => {
-    // scope에 offline_access가 없을 때, 유저가 이미 인가를 한 경우 인가 페이지 패스
-    const checkConsent = async () => {
-      if (!scopes.includes('offline_access') && !prompt) {
-        const { data, error } = await getUserConsent();
+  useAuthorize({ authorize, client });
 
-        if (!data || error) throw error;
-        if (data.list.some((c) => c.clientUuid === clientId)) {
-          authorize();
-          return;
-        }
-      }
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="flex w-full max-w-[400px] flex-col px-5 py-8">
+        <LoadingOverlay show={form.formState.isSubmitting}>
+          <div className="text-title-1 text-pretty whitespace-pre-wrap">
+            {t('authorize.title', { client: client.name })}
+          </div>
+          <div className="h-8" />
+          <div className="text-body-2 text-pretty text-neutral-800">
+            {t('authorize.description', { client: client.name })}
+          </div>
+        </LoadingOverlay>
+        <div className="h-1" />
+        <FormProvider {...form}>
+          <form
+            onSubmit={async (e) => {
+              await onSubmit(e);
+              authorize();
+            }}
+          >
+            <AuthorizeForm client={client} />
+          </form>
+        </FormProvider>
+      </div>
+    </div>
+  );
+};
 
-      setLoading(false);
-    };
+export function AuthorizeFrame() {
+  const { clientId } = useLoaderData({ from: '/_auth-required/authorize' });
+  const { client } = useClient(clientId);
 
-    checkConsent();
-  }, [authorize, clientId, prompt, scopes]);
-
-  // TODO: Loading, Error 상태에 대한 UI를 추가해야 함
-  if (isLoading || !client) {
+  if (!client) {
     return (
       <div className="flex min-h-screen items-center justify-center"></div>
     );
   } else {
-    return (
-      <div className="flex min-h-screen items-center justify-center">
-        <div className="flex w-full max-w-[400px] flex-col px-5 py-8">
-          <LoadingOverlay show={form.formState.isSubmitting}>
-            <div className="text-title-1 text-pretty whitespace-pre-wrap">
-              {t('authorize.title', { client: client.name })}
-            </div>
-            <div className="h-8" />
-            <div className="text-body-2 text-pretty text-neutral-800">
-              {t('authorize.description', { client: client.name })}
-            </div>
-          </LoadingOverlay>
-          <div className="h-1" />
-          <FormProvider {...form}>
-            <form
-              onSubmit={async (e) => {
-                await onSubmit(e);
-                authorize();
-              }}
-            >
-              <AuthorizeForm client={client} />
-            </form>
-          </FormProvider>
-        </div>
-      </div>
-    );
+    <Inner client={client} />;
   }
 }
