5103e1061af04764b9ff37ee8b933ab257056eb0
2paperstar <lee@paperst.ar>
2025-05-04 00:36:22 +0900

    feat: add maintenance banner

--- src/app.tsx
+++ src/app.tsx
@@ -11,7 +11,20 @@ declare module '@tanstack/react-router' {
 }
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <div className="fixed top-0 right-0 left-0 z-50 bg-blue-500 p-4 text-sm text-white">
+        <p>
+          GIST 메일로 로그인 서비스의 점검이 진행중입니다.
+          <br />
+          점검 중에는 로그인 및 인증 서비스의 사용이 제한될 수 있습니다.
+          <br />
+          (예상 점검 시간: 2025.05.04 00:00 ~ 04:00)
+        </p>
+      </div>
+      <RouterProvider router={router} />
+    </>
+  );
 };
 
 export default App;
