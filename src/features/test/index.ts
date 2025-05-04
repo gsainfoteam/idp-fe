ed4d50fba52da5c8b56f52cf0b4fbe8411cca645
Hyunseung Ryu <rhseungg@gmail.com>
2025-05-04 23:30:10 +0900

    remove: unused test page

--- src/features/test/index.ts
+++ src/features/test/index.ts
@@ -1,5 +1,4 @@
 export * from './frames/components/button-test-frame';
 export * from './frames/components/multi-state-switch-test-frame';
 export * from './frames/components/input-test-frame';
-export * from './frames/component-test-frame';
 export * from './frames/funnel-test-frame';
