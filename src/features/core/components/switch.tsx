9bf9e1dd603d06ff5680e66d9a68001a5a70a4f9
Hyunseung Ryu <rhseungg@gmail.com>
2025-05-06 09:27:46 +0900

    feat: add transition in switch

--- src/features/core/components/switch.tsx
+++ src/features/core/components/switch.tsx
@@ -7,31 +7,34 @@ export const Switch = forwardRef<
   React.InputHTMLAttributes<HTMLInputElement>
 >(({ disabled = false, className, ...props }, ref) => {
   return (
-    <label className="flex h-fit w-fit items-center">
-      <input
-        type="checkbox"
-        className={cn('peer appearance-none', className)}
-        ref={ref}
-        disabled={disabled}
-        {...props}
-      />
-      <div
-        className={cn(
-          'flex h-7.5 w-12.5 rounded-full p-0.5',
-          'peer-checked:bg-primary-500 bg-neutral-200',
-          'justify-start peer-checked:justify-end',
-          disabled
-            ? 'cursor-default bg-neutral-300 peer-checked:bg-neutral-300 peer-checked:[&>div]:bg-white'
-            : 'cursor-pointer',
-        )}
-      >
+    <div className="w-fit cursor-pointer">
+      <label className="inline-flex items-center">
+        <input
+          type="checkbox"
+          className={cn('peer sr-only', className)}
+          ref={ref}
+          disabled={disabled}
+          {...props}
+        />
+        {/* 트랙 */}
         <div
           className={cn(
-            'size-6.5 rounded-full',
-            disabled ? 'bg-neutral-500' : 'bg-white',
+            'relative h-7.5 w-12.5 rounded-full p-0.5 transition-colors duration-200',
+            'peer-checked:bg-primary-500 bg-neutral-200 peer-checked:[&>div]:translate-x-5',
+            disabled
+              ? 'cursor-default bg-neutral-300 peer-checked:bg-neutral-300 peer-checked:[&>div]:bg-white'
+              : 'cursor-pointer',
           )}
-        />
-      </div>
-    </label>
+        >
+          {/* 핸들 */}
+          <div
+            className={cn(
+              'absolute top-0.5 left-0.5 size-6.5 rounded-full transition-transform duration-200 ease-in-out',
+              disabled ? 'bg-neutral-500' : 'bg-white',
+            )}
+          />
+        </div>
+      </label>
+    </div>
   );
 });
