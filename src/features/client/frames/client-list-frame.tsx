8cbb6f3690ba0d543648005c71f7627eef698935
2paperstar <lee@paperst.ar>
2025-05-08 01:45:28 +0900

    feat: client list empty page

--- src/features/client/frames/client-list-frame.tsx
+++ src/features/client/frames/client-list-frame.tsx
@@ -1,17 +1,29 @@
 import { useTranslation } from 'react-i18next';
 
+import puzzleImage from '@/assets/icons/color/puzzle.png';
+import { Button, FunnelLayout } from '@/features/core';
+
 export function ClientListFrame() {
   const { t } = useTranslation();
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <div className="flex w-full max-w-[400px] flex-col px-5 py-6">
-        <div className="flex flex-col gap-2">
-          <div className="text-title-1">{t('services.list.title')}</div>
-          <div className="text-body-1 text-neutral-600">
-            {t('services.list.description')}
+    <FunnelLayout
+      stepTitle={t('services.list.title')}
+      title={t('profile.developer')}
+      description={t('services.list.description')}
+      button={
+        <Button variant="primary" className="w-full">
+          {t('services.list.add')}
+        </Button>
+      }
+    >
+      <div className="flex h-full flex-col justify-center">
+        <div className="flex flex-col items-center">
+          <img src={puzzleImage} className="size-[100px] opacity-40" />
+          <div className="text-body-1 text-center whitespace-pre-wrap text-neutral-600">
+            {t('services.list.empty')}
           </div>
         </div>
       </div>
-    </div>
+    </FunnelLayout>
   );
 }
