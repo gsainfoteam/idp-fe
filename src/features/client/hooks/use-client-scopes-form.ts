34c9807e05da61fde037a386cd527e3d04aa1765
2paperstar <lee@paperst.ar>
2025-05-08 22:40:40 +0900

    feat: update client data

--- src/features/client/hooks/use-client-scopes-form.ts
+++ src/features/client/hooks/use-client-scopes-form.ts
@@ -5,6 +5,7 @@ import { z } from 'zod';
 
 import { Client } from './use-client';
 
+import { patchClient } from '@/data/patch-client';
 import { ClientScopeEnum } from '@/routes/_auth-required/authorize';
 
 const schema = z.object({
@@ -29,8 +30,20 @@ export const useClientScopesForm = (client: Client) => {
   const values = useWatch({ control: form.control });
 
   useEffect(() => {
-    console.log(values);
-  }, [values]);
+    const timer = setTimeout(async () => {
+      await patchClient(client.clientId, {
+        scopes: Object.entries(values.scopes ?? {})
+          .filter(([, value]) => value === 'required')
+          .map(([key]) => key),
+        optionalScopes: Object.entries(values.scopes ?? {})
+          .filter(([, value]) => value === 'optional')
+          .map(([key]) => key),
+        idTokenAllowed: values.idTokenAllowed,
+      });
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, [client.clientId, values]);
 
   return { form };
 };
