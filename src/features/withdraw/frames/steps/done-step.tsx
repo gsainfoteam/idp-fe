ab027de32d80cc904246f7d5a5925461db0366a8
2paperstar <lee@paperst.ar>
2025-05-12 13:28:33 +0900

    feat: done step with thumbs up image

--- src/features/withdraw/frames/steps/done-step.tsx
+++ src/features/withdraw/frames/steps/done-step.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from 'react-i18next';
 
+import thumbsUpSrc from '@/assets/icons/color/thumbs-up.png';
 import { Button, FunnelLayout } from '@/features/core';
 
 export function DoneStep({ onNext }: { onNext: () => void }) {
@@ -16,7 +17,13 @@ export function DoneStep({ onNext }: { onNext: () => void }) {
         </Button>
       }
     >
-      <div className="absolute inset-0 flex flex-col items-center justify-center"></div>
+      <div className="absolute inset-0 flex flex-col items-center justify-center">
+        <img
+          src={thumbsUpSrc}
+          alt="thumbs-up"
+          className="size-[125px] opacity-40"
+        />
+      </div>
     </FunnelLayout>
   );
 }
