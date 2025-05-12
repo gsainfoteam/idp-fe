86cd812227b066fb82045843b06fab0bf7c2699e
2paperstar <lee@paperst.ar>
2025-05-12 11:41:01 +0900

    feat: delete user api

---
+++ src/data/delete-user.ts
@@ -0,0 +1,34 @@
+import type { ErrorStatus } from 'openapi-typescript-helpers';
+
+import { paths } from '@/@types/api-schema';
+import { api } from '@/features/core';
+
+enum DeleteUserStatus {
+  INVALID_PASSWORD = 403,
+  SERVER_ERROR = 500,
+}
+
+export const deleteUser = async ({ password }: { password: string }) => {
+  try {
+    const { data } = await api.DELETE('/user', {
+      body: { password },
+    });
+
+    return { data };
+  } catch (err) {
+    if (err instanceof Response) {
+      const status = err.status as Extract<
+        keyof paths['/user']['delete']['responses'],
+        ErrorStatus
+      >;
+
+      return {
+        status: DeleteUserStatus[status] as keyof typeof DeleteUserStatus,
+      };
+    }
+
+    return {
+      status: 'UNKNOWN_ERROR' as const,
+    };
+  }
+};
