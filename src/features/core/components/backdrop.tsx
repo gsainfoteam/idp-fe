983fb59c5a677e16705f84213ccdb74ec6c7b738
Hyunseung Ryu <rhseungg@gmail.com>
2025-05-16 20:55:54 +0900

    feat: separate backdrop click/close

--- src/features/core/components/backdrop.tsx
+++ src/features/core/components/backdrop.tsx
@@ -1,35 +1,56 @@
-import { PropsWithChildren } from 'react';
-import { motion, AnimatePresence, HTMLMotionProps } from 'framer-motion';
+import { PropsWithChildren, useEffect } from 'react';
+import { motion, HTMLMotionProps, AnimatePresence } from 'framer-motion';
 
 import { cn } from '../utils/cn';
 
 interface BackdropProps extends Omit<HTMLMotionProps<'div'>, 'ref'> {
   open: boolean;
+  onClose: () => void;
 }
 
 export function Backdrop({
   open,
+  onClose,
   className,
+  onClick,
   children,
   ...props
 }: PropsWithChildren<BackdropProps>) {
+  useEffect(() => {
+    const handleEscKey = (event: KeyboardEvent) => {
+      if (event.key === 'Escape' && open) {
+        onClose();
+      }
+    };
+
+    window.addEventListener('keydown', handleEscKey);
+    return () => window.removeEventListener('keydown', handleEscKey);
+  }, [open, onClose]);
+
   return (
     <AnimatePresence>
       {open && (
         <motion.div
+          key="backdrop"
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          transition={{ duration: 0.2 }}
+          transition={{
+            duration: 0.15,
+          }}
           className={cn(
             'bg-dimmed-50 fixed inset-0 z-50 h-full w-full',
             className,
           )}
+          onClick={(e) => {
+            onClick?.(e);
+            onClose();
+          }}
           {...props}
         >
           <div className="relative flex h-full w-full items-center justify-center">
             <div className="h-fit w-fit" onClick={(e) => e.stopPropagation()}>
-              {children}
+              <AnimatePresence propagate>{children}</AnimatePresence>
             </div>
           </div>
         </motion.div>
