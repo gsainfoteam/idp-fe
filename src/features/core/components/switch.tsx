0e315897dee54d411d52a38907d09825604cf4c9
Hyunseung Ryu <rhseungg@gmail.com>
2025-05-20 01:42:47 +0900

    feat: switch component customize

--- src/features/core/components/switch.tsx
+++ src/features/core/components/switch.tsx
@@ -2,37 +2,46 @@ import { forwardRef } from 'react';
 
 import { cn } from '../utils/cn';
 
-export const Switch = forwardRef<
-  HTMLInputElement,
-  React.InputHTMLAttributes<HTMLInputElement>
->(({ disabled = false, className, ...props }, ref) => {
-  return (
-    <label className="inline-flex items-center">
-      <input
-        type="checkbox"
-        className={cn('peer sr-only', className)}
-        ref={ref}
-        disabled={disabled}
-        {...props}
-      />
-      {/* 트랙 */}
-      <div
-        className={cn(
-          'relative h-7.5 w-12.5 rounded-full p-0.5 transition-colors duration-200',
-          'peer-checked:bg-primary-500 bg-neutral-200 peer-checked:[&>div]:translate-x-5',
-          disabled
-            ? 'cursor-default bg-neutral-300 peer-checked:bg-neutral-300 peer-checked:[&>div]:bg-white'
-            : 'cursor-pointer',
-        )}
-      >
-        {/* 핸들 */}
+interface SwitchProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  trackClassName?: string;
+  handleClassName?: string;
+}
+
+export const Switch = forwardRef<HTMLInputElement, SwitchProps>(
+  (
+    { disabled = false, className, trackClassName, handleClassName, ...props },
+    ref,
+  ) => {
+    return (
+      <label className="inline-flex items-center">
+        <input
+          type="checkbox"
+          className={cn('peer sr-only', className)}
+          ref={ref}
+          disabled={disabled}
+          {...props}
+        />
+        {/* Track */}
         <div
           className={cn(
-            'absolute top-0.5 left-0.5 size-6.5 rounded-full transition-transform duration-200 ease-in-out',
-            disabled ? 'bg-neutral-500' : 'bg-white',
+            'relative h-7.5 w-12.5 rounded-full p-0.5 transition-colors duration-200',
+            'peer-checked:bg-primary-500 bg-neutral-200 peer-checked:[&>div]:translate-x-5',
+            disabled
+              ? 'cursor-default bg-neutral-300 peer-checked:bg-neutral-300 peer-checked:[&>div]:bg-white'
+              : 'cursor-pointer',
+            trackClassName,
           )}
-        />
-      </div>
-    </label>
-  );
-});
+        >
+          {/* Handle */}
+          <div
+            className={cn(
+              'absolute top-0.5 left-0.5 size-6.5 rounded-full transition-transform duration-200 ease-in-out',
+              disabled ? 'bg-neutral-500' : 'bg-white',
+              handleClassName,
+            )}
+          />
+        </div>
+      </label>
+    );
+  },
+);
