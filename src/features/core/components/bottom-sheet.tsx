5bc66240ce5d57e8fa3595263abbc26fcfe6e04c
Hyunseung Ryu <rhseungg@gmail.com>
2025-05-19 22:45:36 +0900

    feat: drag bottom sheet

--- src/features/core/components/bottom-sheet.tsx
+++ src/features/core/components/bottom-sheet.tsx
@@ -1,5 +1,5 @@
-import { PropsWithChildren } from 'react';
-import { motion } from 'framer-motion';
+import { PropsWithChildren, useEffect } from 'react';
+import { motion, PanInfo, useAnimationControls } from 'framer-motion';
 
 import { cn } from '../utils/cn';
 
@@ -16,20 +16,40 @@ export function BottomSheet({
   children,
   className,
 }: PropsWithChildren<BottomSheetProps>) {
+  const controls = useAnimationControls();
+
+  useEffect(() => {
+    if (open) controls.start({ y: 0 });
+  }, [open, controls]);
+
+  const handleDragEnd = (
+    _: MouseEvent | TouchEvent | PointerEvent,
+    info: PanInfo,
+  ) => {
+    const threshold = 100;
+
+    if (info.offset.y > threshold) onClose();
+    else controls.start({ y: 0 });
+  };
+
   return (
     <Backdrop open={open} onClose={onClose}>
       <motion.div
-        initial={{ y: '150%' }}
-        animate={{ y: 0 }}
-        exit={{ y: '150%' }}
+        initial={{ y: '100%' }}
+        animate={controls}
+        exit={{ y: '100%' }}
         transition={{
           type: 'spring',
           duration: 0.5,
           damping: 20,
           stiffness: 175,
         }}
+        drag="y"
+        dragConstraints={{ top: 0, bottom: 300 }}
+        dragElastic={0.2}
+        onDragEnd={handleDragEnd}
         className={cn(
-          'absolute right-0 bottom-0 left-0 mx-3 mb-3 flex flex-col rounded-[20px] bg-white px-5 pt-9 pb-5',
+          'fixed inset-x-0 bottom-0 z-50 mx-3 mb-3 flex flex-col rounded-[20px] bg-white px-5 pt-9 pb-5 shadow-xl',
           className,
         )}
       >
