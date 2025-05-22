f0176af27ca7bce289e5c30f2a96a83a14c15a2f
Hyunseung Ryu <rhseungg@gmail.com>
2025-05-23 04:21:03 +0900

    refactor: extend button test

--- src/features/core/components/button.stories.tsx
+++ src/features/core/components/button.stories.tsx
@@ -2,6 +2,8 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import { Button } from './button';
 
+import EditIcon from '@/assets/icons/solid/edit.svg?react';
+
 const meta = {
   component: Button,
 } satisfies Meta<typeof Button>;
@@ -10,42 +12,94 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
-  args: {
-    variant: 'primary',
-    disabled: false,
-    children: 'Button',
-  },
-};
-
-export const Secondary: Story = {
-  args: {
-    variant: 'secondary',
-    disabled: false,
-    children: 'Button',
-  },
-};
-
 export const Default: Story = {
   args: {
     variant: 'default',
+    loading: false,
     disabled: false,
-    children: 'Button',
   },
+  render: () => <ButtonGroup loading={false} disabled={false} />,
 };
 
-export const Text: Story = {
+export const Loading: Story = {
   args: {
-    variant: 'text',
+    variant: 'default',
+    loading: true,
     disabled: false,
-    children: 'Button',
   },
+  render: () => <ButtonGroup loading={true} disabled={false} />,
 };
 
-export const Link: Story = {
+export const Disabled: Story = {
   args: {
-    variant: 'link',
-    disabled: false,
-    children: 'Button',
+    variant: 'default',
+    loading: false,
+    disabled: true,
   },
+  render: () => <ButtonGroup loading={false} disabled={true} />,
 };
+
+const ButtonGroup = ({
+  loading,
+  disabled,
+}: {
+  loading: boolean;
+  disabled: boolean;
+}) => (
+  <div className="flex flex-col gap-4">
+    <div className="flex gap-4">
+      <Button variant="default" loading={loading} disabled={disabled}>
+        Default
+      </Button>
+      <Button variant="primary" loading={loading} disabled={disabled}>
+        Primary
+      </Button>
+      <Button variant="secondary" loading={loading} disabled={disabled}>
+        Secondary
+      </Button>
+      <Button variant="text" loading={loading} disabled={disabled}>
+        Text
+      </Button>
+      <Button variant="link" loading={loading} disabled={disabled}>
+        Link
+      </Button>
+    </div>
+    <div className="flex gap-4">
+      <Button
+        variant="default"
+        className="px-3"
+        prefixIcon={<EditIcon />}
+        loading={loading}
+        disabled={disabled}
+      />
+      <Button
+        variant="primary"
+        className="px-3"
+        prefixIcon={<EditIcon />}
+        loading={loading}
+        disabled={disabled}
+      />
+      <Button
+        variant="secondary"
+        className="px-3"
+        prefixIcon={<EditIcon />}
+        loading={loading}
+        disabled={disabled}
+      />
+      <Button
+        variant="text"
+        className="px-3"
+        prefixIcon={<EditIcon />}
+        loading={loading}
+        disabled={disabled}
+      />
+      <Button
+        variant="link"
+        className="px-3"
+        prefixIcon={<EditIcon />}
+        loading={loading}
+        disabled={disabled}
+      />
+    </div>
+  </div>
+);
