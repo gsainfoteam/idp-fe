5e058c05595582817834a14db50920f0fb9067e0
Hyunseung Ryu <rhseungg@gmail.com>
2025-05-23 04:20:27 +0900

    feat: change outer border to inner border

--- src/features/core/components/button.tsx
+++ src/features/core/components/button.tsx
@@ -17,11 +17,11 @@ const buttonStyle = cva(
     variants: {
       variant: {
         default:
-          'rounded-lg border border-neutral-200 bg-neutral-50 px-5 py-3 text-black active:bg-neutral-100 disabled:border-neutral-300 disabled:bg-neutral-200 disabled:text-neutral-500',
+          'rounded-lg bg-neutral-50 px-5 py-3 text-black [box-shadow:inset_0_0_0_1px_var(--color-neutral-200)] active:bg-neutral-100 disabled:bg-neutral-200 disabled:text-neutral-500 disabled:[box-shadow:inset_0_0_0_1px_var(--color-neutral-300)]',
         primary:
           'bg-primary-600 active:bg-primary-700 rounded-lg px-5 py-3 text-white disabled:bg-neutral-600',
         secondary:
-          'text-primary-600 border-primary-600 active:bg-primary-50 rounded-lg border bg-white px-5 py-3 disabled:border-neutral-600 disabled:bg-neutral-100 disabled:text-neutral-600',
+          'text-primary-600 active:bg-primary-50 rounded-lg bg-white px-5 py-3 [box-shadow:inset_0_0_0_1px_var(--color-primary-600)] disabled:bg-neutral-100 disabled:text-neutral-600 disabled:[box-shadow:inset_0_0_0_1px_var(--color-neutral-600)]',
         text: 'text-primary-600 active:text-primary-700 px-5 py-3 disabled:text-neutral-600',
         link: 'text-body-1 text-neutral-400 underline active:text-neutral-600 disabled:cursor-default disabled:text-neutral-200',
       },
@@ -94,15 +94,7 @@ export function Button({
       {/* loading ellipse */}
       {showEllipse && (
         <div className="absolute left-1/2 -translate-x-1/2">
-          <LoadingEllipse
-            className={cn(
-              variant === 'default'
-                ? 'bg-neutral-600'
-                : variant === 'primary'
-                  ? 'bg-neutral-200'
-                  : 'bg-primary-600',
-            )}
-          />
+          <LoadingEllipse className="bg-current" />
         </div>
       )}
     </button>
