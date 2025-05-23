5bc5b76514fabcedc2f2bafbbf5ed43c01565b37
Hyunseung Ryu <rhseungg@gmail.com>
2025-05-24 01:43:24 +0900

    refactor: change button disabled color (#131)

--- src/features/core/components/button.tsx
+++ src/features/core/components/button.tsx
@@ -17,12 +17,12 @@ const buttonStyle = cva(
     variants: {
       variant: {
         default:
-          'rounded-lg border border-neutral-200 bg-neutral-50 px-5 py-3 text-black active:bg-neutral-100 disabled:border-neutral-300 disabled:bg-neutral-200 disabled:text-neutral-500',
+          'rounded-lg border border-neutral-200 bg-neutral-50 px-5 py-3 text-black active:bg-neutral-100 disabled:border-neutral-300 disabled:bg-neutral-200 disabled:text-neutral-300',
         primary:
-          'bg-primary-600 active:bg-primary-700 rounded-lg px-5 py-3 text-white disabled:bg-neutral-600',
+          'bg-primary-600 active:bg-primary-700 rounded-lg px-5 py-3 text-white disabled:bg-neutral-200',
         secondary:
-          'text-primary-600 border-primary-600 active:bg-primary-50 rounded-lg border bg-white px-5 py-3 disabled:border-neutral-600 disabled:bg-neutral-100 disabled:text-neutral-600',
-        text: 'text-primary-600 active:text-primary-700 px-5 py-3 disabled:text-neutral-600',
+          'text-primary-600 border-primary-600 active:bg-primary-50 rounded-lg border bg-white px-5 py-3 disabled:border-neutral-200 disabled:bg-neutral-50 disabled:text-neutral-200',
+        text: 'text-primary-600 active:text-primary-700 px-5 py-3 disabled:text-neutral-200',
         link: 'text-body-1 text-neutral-400 underline active:text-neutral-600 disabled:cursor-default disabled:text-neutral-200',
       },
       loading: {
