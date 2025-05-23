a40495f4a6017e52dac65cf64315fde320d9feb8
Hyunseung Ryu <rhseungg@gmail.com>
2025-05-24 00:20:54 +0900

    fix: add missing img prop on avatar (#130)

--- src/features/withdraw/frames/steps/confirm-step.tsx
+++ src/features/withdraw/frames/steps/confirm-step.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from 'react-i18next';
 import { useConfirmWithdrawalForm } from '../../hooks/steps/use-confirm-withdrawal-form';
 
 import { Avatar, Button, FunnelLayout, uniqueKey } from '@/features/core';
+import { useAuth } from '@/features/auth';
 
 export function ConfirmStep({
   onNext,
@@ -13,10 +14,14 @@ export function ConfirmStep({
   context: { password: string; email: string; name: string; studentId: string };
 }) {
   const { t } = useTranslation();
-  const { consents, isLoading, onSubmit } = useConfirmWithdrawalForm({
+  const { user } = useAuth();
+  const { clients, isLoading, onSubmit } = useConfirmWithdrawalForm({
     onNext,
     context,
   });
+
+  // impossible to happen
+  if (!user) throw new Error('User not found');
 
   return (
     <form onSubmit={onSubmit}>
@@ -24,8 +29,8 @@ export function ConfirmStep({
         title={t('withdraw.title')}
         stepTitle={t('withdraw.steps.confirm.title')}
         description={
-          consents
-            ? consents.length
+          clients
+            ? clients.length
               ? t('withdraw.steps.confirm.description')
               : t('withdraw.steps.confirm.description_without_services')
             : undefined
@@ -46,7 +51,7 @@ export function ConfirmStep({
             <Button
               variant="primary"
               className="w-full"
-              disabled={!consents}
+              disabled={!clients}
               loading={isLoading}
             >
               {t('withdraw.steps.confirm.button')}
@@ -56,22 +61,23 @@ export function ConfirmStep({
       >
         <div className="flex flex-col gap-3">
           {/* TODO: add loading and error */}
-          {consents ? (
-            consents.length ? (
-              consents.map((consent) => (
+          {clients ? (
+            clients.length ? (
+              clients.map((client) => (
                 <div
                   className="flex items-center gap-3 rounded-lg border border-neutral-100 p-3"
-                  key={consent.clientUuid}
+                  key={client.clientId}
                 >
                   <Avatar
                     size={10}
-                    name={consent.client.name}
-                    seed={uniqueKey(consent.client.uuid)}
+                    img={client.picture ?? undefined}
+                    name={client.name}
+                    seed={uniqueKey(client.clientId)}
                     className="text-title-1 rounded-lg"
                   />
                   <div className="flex-1">
                     <div className="text-title-3 text-neutral-900">
-                      {consent.client.name}
+                      {client.name}
                     </div>
                   </div>
                 </div>
@@ -80,6 +86,7 @@ export function ConfirmStep({
               <div className="absolute inset-0 flex flex-col items-center justify-center">
                 <Avatar
                   size={32}
+                  img={user.picture ?? undefined}
                   name={context.name}
                   seed={uniqueKey(context.studentId.toString())}
                 />
