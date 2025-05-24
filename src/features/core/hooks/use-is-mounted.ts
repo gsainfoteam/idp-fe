7bffa2a4bca33fa9260575b75755b2c3de42ae45
Hyunseung Ryu <rhseungg@gmail.com>
2025-05-25 03:37:16 +0900

    fix: initial value of ref

--- src/features/core/hooks/use-is-mounted.ts
+++ src/features/core/hooks/use-is-mounted.ts
@@ -1,14 +1,14 @@
 import { useEffect, useRef } from 'react';
 
 export function useIsMounted() {
-  const isMounted = useRef(true);
+  const isMounted = useRef(false);
 
   useEffect(() => {
     isMounted.current = true;
     return () => {
       isMounted.current = false;
     };
-  }, [isMounted]);
+  }, []);
 
   const getIsMounted = () => isMounted.current;
 
