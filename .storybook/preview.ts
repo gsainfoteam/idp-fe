45557cb19394cac33fb166779cb42e78f797b8db
Cyan <hoony6134@gmail.com>
2025-05-25 15:29:29 +0900

    feat: integrated dark mode switcher to storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,18 @@
 import type { Preview } from '@storybook/react';
+import { withThemeByClassName } from '@storybook/addon-themes';
+
 import '../src/index.css';
 
 const preview: Preview = {
+  decorators: [
+    withThemeByClassName({
+      themes: {
+        light: '',
+        dark: 'dark',
+      },
+      defaultTheme: 'light',
+    }),
+  ],
   parameters: {
     controls: {
       matchers: {
@@ -13,3 +24,26 @@ const preview: Preview = {
 };
 
 export default preview;
+
+/*
+// Replace your-renderer with the framework you are using (e.g., react, vue3)
+import { Preview, Renderer } from '@storybook/your-renderer';
+import { withThemeByDataAttribute } from '@storybook/addon-themes';
+
+import '../src/index.css'; // Your application's global CSS file
+
+const preview: Preview = {
+  decorators: [
+    withThemeByDataAttribute<Renderer>({
+      themes: {
+        light: 'light',
+        dark: 'dark',
+      },
+      defaultTheme: 'light',
+      attributeName: 'data-theme',
+    }),
+  ],
+};
+
+export default preview;
+*/
