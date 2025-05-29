04195f85c9b7d69cac22ffd3256b8262cc092f92
Hyunseung Ryu <rhseungg@gmail.com>
2025-05-29 09:06:36 +0900

    refactor: add sub component of bottom sheet

--- src/features/core/components/bottom-sheet.stories.tsx
+++ src/features/core/components/bottom-sheet.stories.tsx
@@ -7,7 +7,7 @@ import { Button } from './button';
 const meta = {
   component: BottomSheet,
   argTypes: {
-    open: {
+    isOpen: {
       options: [true, false],
       control: { type: 'boolean' },
     },
@@ -18,7 +18,7 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-const BottomSheetWithState = ({ open: initialOpen }: { open: boolean }) => {
+const BottomSheetWithState = ({ isOpen: initialOpen }: { isOpen: boolean }) => {
   const [open, setOpen] = useState(initialOpen);
 
   useEffect(() => {
@@ -26,43 +26,40 @@ const BottomSheetWithState = ({ open: initialOpen }: { open: boolean }) => {
   }, [initialOpen]);
 
   return (
-    <BottomSheet open={open} onClose={() => setOpen(false)}>
-      <div className="text-title-1 w-full text-pretty text-neutral-950">
-        Title must be so long
-      </div>
-      <div className="text-body-1 mt-2 w-full text-pretty text-neutral-600">
+    <BottomSheet isOpen={open} close={() => setOpen(false)}>
+      <BottomSheet.Header>Title must be so long</BottomSheet.Header>
+      <BottomSheet.Body>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris non
         nulla vitae augue pellentesque mollis.
-      </div>
-      <div className="mt-10 flex w-full justify-end gap-3">
-        <Button
-          variant="secondary"
-          onClick={() => setOpen(false)}
-          className="w-full"
-        >
-          Close
-        </Button>
-        <Button
-          variant="primary"
-          onClick={() => {
-            alert('Success!');
-            setOpen(false);
-          }}
-          className="w-full"
-        >
-          Success
-        </Button>
-      </div>
+      </BottomSheet.Body>
+      <BottomSheet.Footer>
+        <BottomSheet.Close>
+          <Button variant="secondary" className="w-full">
+            Close
+          </Button>
+        </BottomSheet.Close>
+        <BottomSheet.Close>
+          <Button
+            variant="primary"
+            onClick={() => {
+              alert('Success!');
+            }}
+            className="w-full"
+          >
+            Success
+          </Button>
+        </BottomSheet.Close>
+      </BottomSheet.Footer>
     </BottomSheet>
   );
 };
 
 export const Default: Story = {
   args: {
-    open: false,
-    onClose: () => {},
+    isOpen: false,
+    close: () => {},
   },
-  render: ({ open }) => {
-    return <BottomSheetWithState open={open} />;
+  render: ({ isOpen }) => {
+    return <BottomSheetWithState isOpen={isOpen} />;
   },
 };
--- src/features/core/components/bottom-sheet.tsx
+++ src/features/core/components/bottom-sheet.tsx
@@ -1,35 +1,39 @@
-import { PropsWithChildren, useEffect } from 'react';
+import { createContext, PropsWithChildren, useContext, useEffect } from 'react';
 import { motion, PanInfo, useAnimationControls } from 'framer-motion';
-
 import { cn } from '../utils/cn';
-
 import { Backdrop } from './backdrop';
 
-interface BottomSheetProps extends React.HTMLAttributes<HTMLDivElement> {
-  open: boolean;
-  onClose: () => void;
+interface BottomSheetContextType {
+  close: () => void;
 }
 
-export function BottomSheet({
-  open,
-  onClose,
+const BottomSheetContext = createContext<BottomSheetContextType | null>(null);
+
+interface BottomSheetProps extends React.HTMLAttributes<HTMLDivElement> {
+  isOpen: boolean;
+  close: () => void;
+}
+
+const BottomSheet = ({
+  isOpen,
+  close,
   children,
   className,
-}: PropsWithChildren<BottomSheetProps>) {
+}: PropsWithChildren<BottomSheetProps>) => {
   const controls = useAnimationControls();
 
   useEffect(() => {
     const handleEscKey = (event: KeyboardEvent) => {
-      if (event.key === 'Escape' && open) onClose();
+      if (event.key === 'Escape' && isOpen) close();
     };
 
     window.addEventListener('keydown', handleEscKey);
     return () => window.removeEventListener('keydown', handleEscKey);
-  }, [open, onClose]);
+  }, [isOpen, close]);
 
   useEffect(() => {
-    if (open) controls.start({ y: 0 });
-  }, [open, controls]);
+    if (isOpen) controls.start({ y: 0 });
+  }, [isOpen, controls]);
 
   const handleDragEnd = (
     _: MouseEvent | TouchEvent | PointerEvent,
@@ -37,34 +41,113 @@ export function BottomSheet({
   ) => {
     const threshold = 100;
 
-    if (info.offset.y > threshold) onClose();
+    if (info.offset.y > threshold) close();
     else controls.start({ y: 0 });
   };
 
   return (
-    <Backdrop open={open} onClose={onClose}>
-      <motion.div
-        initial={{ y: '100%' }}
-        animate={controls}
-        exit={{ y: '100%' }}
-        transition={{
-          type: 'spring',
-          duration: 0.5,
-          damping: 20,
-          stiffness: 175,
-        }}
-        drag="y"
-        dragConstraints={{ top: 0, bottom: 300 }}
-        dragElastic={0.2}
-        onDragEnd={handleDragEnd}
-        className={cn(
-          'bg-bottom-sheet-background shadow-bottom-sheet-shadow fixed inset-x-0 bottom-0 z-50 mx-3 mb-3 flex flex-col rounded-[20px] px-5 pt-9 pb-5 shadow-xl',
-          className,
-        )}
-      >
-        <div className="bg-bottom-sheet-handle absolute top-2 left-1/2 h-1.5 w-12.5 -translate-x-1/2 rounded-full" />
-        {children}
-      </motion.div>
-    </Backdrop>
+    <BottomSheetContext.Provider value={{ close }}>
+      <Backdrop isOpen={isOpen} close={close}>
+        <motion.div
+          initial={{ y: '100%' }}
+          animate={controls}
+          exit={{ y: '100%' }}
+          transition={{
+            type: 'spring',
+            duration: 0.5,
+            damping: 20,
+            stiffness: 175,
+          }}
+          drag="y"
+          dragConstraints={{ top: 0, bottom: 300 }}
+          dragElastic={0.2}
+          onDragEnd={handleDragEnd}
+          className={cn(
+            'bg-bottom-sheet-background fixed inset-x-0 bottom-0 z-50 mx-3 mb-3 flex flex-col rounded-[20px] pt-9 shadow-xl',
+            className,
+          )}
+        >
+          <div className="bg-bottom-sheet-handle absolute top-2 left-1/2 h-1.5 w-12.5 -translate-x-1/2 rounded-full" />
+          {children}
+        </motion.div>
+      </Backdrop>
+    </BottomSheetContext.Provider>
   );
-}
+};
+
+BottomSheet.Header = ({
+  children,
+  className,
+  ...props
+}: PropsWithChildren<React.HTMLAttributes<HTMLDivElement>>) => {
+  return (
+    <div
+      className={cn(
+        'text-title-1 w-full px-5 pb-3 text-pretty text-neutral-950',
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+BottomSheet.Body = ({
+  children,
+  className,
+  ...props
+}: PropsWithChildren<React.HTMLAttributes<HTMLDivElement>>) => {
+  return (
+    <div
+      className={cn(
+        'text-body-1 w-full px-5 pb-3 text-pretty text-neutral-600',
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+BottomSheet.Footer = ({
+  children,
+  className,
+  ...props
+}: PropsWithChildren<React.HTMLAttributes<HTMLDivElement>>) => {
+  return (
+    <div
+      className={cn('flex w-full justify-end gap-3 px-5 pt-5 pb-5', className)}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+BottomSheet.Close = ({
+  children,
+  className,
+  onClick,
+  ...props
+}: PropsWithChildren<React.HTMLAttributes<HTMLDivElement>>) => {
+  const context = useContext(BottomSheetContext);
+  if (!context)
+    throw new Error('BottomSheet.Close must be used within a BottomSheet');
+
+  return (
+    <div
+      onClick={(e) => {
+        context.close();
+        onClick?.(e);
+      }}
+      className={cn('w-full cursor-pointer', className)}
+      {...props}
+    >
+      {children}
+    </div>
+  );
+};
+
+export { BottomSheet };
