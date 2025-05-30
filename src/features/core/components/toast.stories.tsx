7ca9702035eca0028590680886d7b6ad5b46bd89
Cyan <65375075+hoony6134@users.noreply.github.com>
2025-05-30 09:01:10 +0900

    feat: dark mode for toast story (#157)

---
+++ src/features/core/components/toast.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import toast, { Toaster } from 'react-hot-toast';
+import { Button } from './button';
+
+const meta = {
+  component: Button,
+  decorators: [
+    (Story) => (
+      <>
+        <Story />
+        <Toaster
+          toastOptions={{
+            style: {
+              background: 'var(--color-toast-background)',
+              color: 'var(--color-toast-text)',
+              border: '1px solid var(--color-toast-border)',
+            },
+          }}
+        />
+      </>
+    ),
+  ],
+} satisfies Meta<typeof Button>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    variant: 'primary',
+    children: 'Show Toast',
+    onClick: () => toast.error('hello'),
+  },
+};
