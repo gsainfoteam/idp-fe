f9d8b33e43d165887ad8d4b687b11dfe3dea9fc8
Hyunseung Ryu <rhseungg@gmail.com>
2025-05-31 23:56:32 +0900

    feat: add color and disabled state to mss

--- src/features/core/components/multi-state-switch.tsx
+++ src/features/core/components/multi-state-switch.tsx
@@ -1,17 +1,41 @@
 import { useEffect, useState } from 'react';
 
-import { cn } from '../utils/cn';
+import { cn, palette } from '@/features/core';
 
 interface MultiStateSwitchProps extends React.HTMLAttributes<HTMLDivElement> {
   labels: string[];
   selected?: number;
+  disabled?: boolean;
   onChangeIndex?: (index: number, label: string) => void;
 }
+
+const mssColors = palette((selected: boolean) => ({
+  default: {
+    background: selected
+      ? 'bg-mss-selected-default-background'
+      : 'bg-mss-default-default-background',
+    border: 'inset-ring-mss-default-default-background inset-ring-2',
+    label: selected
+      ? 'text-mss-selected-default-label'
+      : 'text-mss-default-default-label',
+  },
+  disabled: {
+    background: selected
+      ? 'disabled:bg-mss-selected-disabled-background'
+      : 'disabled:bg-mss-default-disabled-background',
+    border: 'disabled:inset-ring-mss-default-disabled-background',
+    label: selected
+      ? 'disabled:text-mss-selected-disabled-label'
+      : 'disabled:text-mss-default-disabled-label',
+  },
+}));
 
 export function MultiStateSwitch({
   labels,
   selected: initialSelected = 0,
+  disabled = false,
   onChangeIndex,
+  className,
   ...props
 }: MultiStateSwitchProps) {
   const [selected, setSelected] = useState(initialSelected);
@@ -23,17 +47,24 @@ export function MultiStateSwitch({
 
   return (
     <div
-      className="bg-multi-state-switch-background flex h-fit w-full rounded-lg"
+      className={cn(
+        'flex h-fit w-full rounded-lg',
+        disabled
+          ? 'bg-mss-default-disabled-background'
+          : 'bg-mss-default-default-background',
+        className,
+      )}
       {...props}
     >
       {labels.map((label, idx) => (
         <button
           key={idx}
+          disabled={disabled}
           className={cn(
-            'flex w-full cursor-pointer items-center justify-center rounded-lg py-3 text-center',
-            idx === selected
-              ? 'text-title-3 bg-multi-state-switch-selected-background text-multi-state-switch-selected-label shadow-[inset_0_0_0_2px_theme(colors.neutral.100)]'
-              : 'text-body-1 text-multi-state-switch-unselected-label bg-transparent',
+            'flex w-full items-center justify-center rounded-lg py-3 text-center',
+            idx === selected ? 'text-title-3' : 'text-body-1',
+            disabled ? 'cursor-default' : 'cursor-pointer',
+            mssColors(idx === selected),
           )}
           onClick={() => {
             setSelected(idx);
