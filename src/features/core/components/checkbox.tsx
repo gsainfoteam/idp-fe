97118171ead84696d8081d5d6e26d87670aa7a46
Hyunseung Ryu <rhseungg@gmail.com>
2025-06-02 01:51:25 +0900

    feat: add checkbox color style

--- src/features/core/components/checkbox.tsx
+++ src/features/core/components/checkbox.tsx
@@ -1,42 +1,63 @@
 import { forwardRef } from 'react';
 
 import CheckIcon from '@/assets/icons/line/checkbox.svg?react';
-import { cn } from '@/features/core';
+import { cn, palette } from '@/features/core';
 
-export const Checkbox = forwardRef<
-  HTMLInputElement,
-  React.InputHTMLAttributes<HTMLInputElement>
->(({ disabled = false, className, children, ...props }, ref) => {
-  return (
-    <div className="w-fit cursor-pointer">
-      <label className="flex w-fit items-center">
-        <input
-          type="checkbox"
-          className={cn('peer appearance-none', className)}
-          ref={ref}
-          disabled={disabled}
-          {...props}
-        />
-        <div
-          className={cn(
-            'm-1 flex size-6 items-center justify-center rounded transition-all',
-            'peer-checked:bg-checkbox-peer-checked-background border-checkbox-border bg-checkbox-background border peer-checked:border-none peer-checked:*:block',
-            disabled
-              ? 'bg-checkbox-disabled-background peer-checked:bg-checkbox-peer-checked-disabled-background cursor-default'
-              : 'cursor-pointer',
-          )}
-        >
-          <CheckIcon className="text-button-label hidden" />
-        </div>
-        <div
-          className={cn(
-            'text-body-1 text-checkbox-label ml-2',
-            disabled ? 'cursor-default' : 'cursor-pointer',
-          )}
-        >
-          {children}
-        </div>
-      </label>
-    </div>
-  );
-});
+export interface CheckboxProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const checkboxColors = palette(() => ({
+  default: {
+    background: 'bg-transparent',
+    border: 'inset-ring-checkbox-default-border',
+    foreground: 'text-transparent',
+  },
+  checked: {
+    background: 'peer-checked:bg-checkbox-checked-background',
+    border: 'peer-checked:inset-ring-checkbox-checked-border',
+    foreground: 'peer-checked:text-checkbox-checked-foreground',
+  },
+  disabled: {
+    background: 'peer-disabled:bg-checkbox-disabled-background',
+    border: cn(
+      'peer-disabled:not-peer-checked:inset-ring-checkbox-default-border',
+      'peer-disabled:peer-checked:inset-ring-checkbox-disabled-border',
+    ),
+    foreground: 'peer-disabled:text-checkbox-disabled-foreground',
+  },
+}));
+
+export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
+  ({ disabled = false, className, children, ...props }, ref) => {
+    return (
+      <div className="w-fit cursor-pointer">
+        <label className="flex w-fit items-center">
+          <input
+            type="checkbox"
+            className={cn('peer appearance-none', className)}
+            ref={ref}
+            disabled={disabled}
+            {...props}
+          />
+          <div
+            className={cn(
+              'm-1 flex size-5 items-center justify-center rounded inset-ring-1 transition-all',
+              'cursor-pointer peer-checked:*:block peer-disabled:cursor-default',
+              checkboxColors(),
+            )}
+          >
+            <CheckIcon className="hidden text-current" />
+          </div>
+          <div
+            className={cn(
+              'text-body-1 text-label ml-2',
+              'cursor-pointer peer-disabled:cursor-default',
+            )}
+          >
+            {children}
+          </div>
+        </label>
+      </div>
+    );
+  },
+);
--- src/stories/checkbox.stories.tsx
+++ src/stories/checkbox.stories.tsx
@@ -1,10 +1,26 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Checkbox } from '@/features/core';
+import { Checkbox, CheckboxProps } from '@/features/core';
+
+const CheckboxWithLabel = ({
+  label,
+  ...props
+}: CheckboxProps & { label: string }) => {
+  return (
+    <div className="bg-funnel-background absolute inset-0">
+      <div className="flex flex-col gap-4 p-4">
+        <Checkbox {...props}>{label}</Checkbox>
+        <Checkbox {...props}>{label}</Checkbox>
+        <Checkbox {...props}>{label}</Checkbox>
+        <Checkbox {...props}>{label}</Checkbox>
+      </div>
+    </div>
+  );
+};
 
 const meta = {
-  component: Checkbox,
-} satisfies Meta<typeof Checkbox>;
+  component: CheckboxWithLabel,
+} satisfies Meta<typeof CheckboxWithLabel>;
 
 export default meta;
 
@@ -12,12 +28,7 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
+    label: 'Checkbox',
     disabled: false,
-  },
-};
-
-export const Disabled: Story = {
-  args: {
-    disabled: true,
   },
 };
