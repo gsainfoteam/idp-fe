bd9856e632085515026fae760544e6061e6a9b9e
Hyunseung Ryu <rhseungg@gmail.com>
2025-06-25 23:37:00 +0900

    feat: add client delete section

---
+++ src/features/client/components/client-delete-form.tsx
@@ -0,0 +1,36 @@
+import { useTranslation } from 'react-i18next';
+
+import { Button } from '@/features/core';
+import { Client } from '../hooks/use-client';
+import TrashBinIcon from '@/assets/icons/solid/trash-bin.svg?react';
+import { useClientDeleteForm } from '../hooks/use-client-delete-form';
+import { useNavigate } from '@tanstack/react-router';
+
+export function ClientDeleteForm({ client }: { client: Client }) {
+  const { t } = useTranslation();
+  const { onSubmit } = useClientDeleteForm(client);
+  const navigate = useNavigate({ from: '/clients/$id' });
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="text-title-3">{t('services.detail.delete.title')}</div>
+      <div className="flex flex-col gap-5">
+        <div className="flex items-center justify-between">
+          // TODO: idf-100 머지되면 variant="warning" 으로 바꾸기
+          <Button
+            variant="primary"
+            disabled={client.deleteRequestedAt != null}
+            prefixIcon={<TrashBinIcon />}
+            className="bg-red-600"
+            onClick={async () => {
+              await onSubmit();
+              await navigate({ to: '/clients' });
+            }}
+          >
+            {t('services.detail.delete.button')}
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
