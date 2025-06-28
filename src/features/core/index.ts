213b2e1d5aafcaf073279450d324e99baaaf404a
Hyunseung Ryu <rhseungg@gmail.com>
2025-06-28 21:59:53 +0900

    feat: add fallback frame

--- src/features/core/index.ts
+++ src/features/core/index.ts
@@ -26,3 +26,4 @@ export * from './hooks/use-theme';
 export * from './hooks/use-is-mounted';
 export * from './hooks/use-loading';
 export * from './hooks/use-copy';
+export * from './frames/not-found-frame';
