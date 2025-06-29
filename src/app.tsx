13b414d1081cdcdfc63a2bcc41c3122b569e73dd
Hyunseung Ryu <rhseungg@gmail.com>
2025-06-30 07:54:51 +0900

    refactor: use overlay-kit (#168)

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
 import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { Toaster } from 'react-hot-toast';
-
+import { OverlayProvider } from 'overlay-kit';
 import { routeTree } from './routeTree.gen';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { NotFoundFrame } from '@/features/core';
-
+        
+const queryClient = new QueryClient();
 const router = createRouter({
   routeTree,
   defaultPreload: 'intent',
@@ -18,10 +20,12 @@ declare module '@tanstack/react-router' {
 
 const App = () => {
   return (
-    <>
-      <RouterProvider router={router} />
-      <Toaster />
-    </>
+    <QueryClientProvider client={queryClient}>
+      <OverlayProvider>
+        <RouterProvider router={router} />
+        <Toaster />
+      </OverlayProvider>
+    </QueryClientProvider>
   );
 };
 
--- src/features/auth/hooks/register-steps/use-email-form.ts
+++ src/features/auth/hooks/register-steps/use-email-form.ts
@@ -7,6 +7,8 @@ import { z } from 'zod';
 import { RegisterSteps } from '../../frames/register-frame';
 
 import { DifferenceNonNullable } from '@/features/core';
+import { postVerifyEmail } from '@/data/post-verify-email';
+import toast from 'react-hot-toast';
 
 const createSchema = (t: TFunction) =>
   z.object({
@@ -20,14 +22,13 @@ const createSchema = (t: TFunction) =>
 
 export const useEmailForm = ({
   onNext,
+  overlay,
 }: {
   context: RegisterSteps['email'];
   onNext: (
-    data: DifferenceNonNullable<
-      RegisterSteps['emailOverlay'],
-      RegisterSteps['email']
-    >,
+    data: DifferenceNonNullable<RegisterSteps['code'], RegisterSteps['email']>,
   ) => void;
+  overlay: (isSubmitting: boolean) => Promise<boolean>;
 }) => {
   const { t } = useTranslation();
   const form = useForm({
@@ -36,7 +37,25 @@ export const useEmailForm = ({
   });
 
   const onSubmit = form.handleSubmit(async (formData) => {
-    onNext(formData);
+    const result = await overlay(form.formState.isSubmitting);
+    if (result === false) return;
+
+    const { status } = await postVerifyEmail({ email: formData.email });
+
+    if (status) {
+      switch (status) {
+        case 'SERVER_ERROR':
+          toast.error(t('toast.server_error'));
+          break;
+        case 'UNKNOWN_ERROR':
+          toast.error(t('toast.unknown_error'));
+          break;
+      }
+
+      return;
+    }
+
+    onNext({ emailAgree: true, ...formData });
   });
 
   return { form, onSubmit };
