b79612b951ac62f02396d358c3e1aeca68dd4095
Hyunseung Ryu <rhseungg@gmail.com>
2025-06-30 13:09:39 +0900

    refactor: navigate logic

--- src/routes/_auth-required/clients/new.tsx
+++ src/routes/_auth-required/clients/new.tsx
@@ -4,7 +4,11 @@ import { ClientAddFrame } from '@/features/client';
 
 const ClientAddPage = () => {
   const navigate = Route.useNavigate();
-  return <ClientAddFrame onSuccess={() => navigate({ to: '/clients' })} />;
+  return (
+    <ClientAddFrame
+      onSuccess={() => navigate({ to: '/clients', replace: true })}
+    />
+  );
 };
 
 export const Route = createFileRoute('/_auth-required/clients/new')({
