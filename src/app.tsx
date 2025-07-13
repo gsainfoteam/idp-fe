82bead7d9c58ac89dbd3ca93a5f66a47600627d2
Hyunseung Ryu <rhseungg@gmail.com>
2025-07-13 14:31:33 +0900

    feat: view transition (#184)

--- src/app.tsx
+++ src/app.tsx
@@ -9,6 +9,19 @@ const router = createRouter({
   routeTree,
   defaultPreload: 'intent',
   defaultNotFoundComponent: () => <NotFoundFrame />,
+  defaultViewTransition: {
+    types: ({ fromLocation, toLocation, hrefChanged }) => {
+      // 시작 페이지이거나, 경로와 쿼리 등이 모두 동일한 새로고침의 경우
+      if (!fromLocation || !hrefChanged) {
+        return ['reload'];
+      }
+
+      const fromIndex = fromLocation.state.__TSR_index;
+      const toIndex = toLocation.state.__TSR_index;
+
+      return [fromIndex > toIndex ? 'backwards' : 'forwards'];
+    },
+  },
 });
 
 declare module '@tanstack/react-router' {
