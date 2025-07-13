14656e580cb206d0bea8ec44625759fc39453648
Hyunseung Ryu <rhseungg@gmail.com>
2025-07-13 12:17:56 +0900

    fix: dynamic gap based on button size

--- src/features/core/components/loading-ellipse.tsx
+++ src/features/core/components/loading-ellipse.tsx
@@ -1,14 +1,22 @@
 import { cn } from '@/features/core';
 
+interface LoadingEllipseProps extends React.HTMLAttributes<HTMLDivElement> {
+  containerClassName?: string;
+}
+
 export function LoadingEllipse({
   className,
+  containerClassName,
   ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
+}: LoadingEllipseProps) {
   return (
-    <div className="flex h-fit w-full items-center gap-2" {...props}>
-      <div className={cn('h-1 w-1 rounded-full', 'animate-ping1', className)} />
-      <div className={cn('h-1 w-1 rounded-full', 'animate-ping2', className)} />
-      <div className={cn('h-1 w-1 rounded-full', 'animate-ping3', className)} />
+    <div
+      className={cn('flex h-fit w-full items-center gap-2', containerClassName)}
+      {...props}
+    >
+      <div className={cn('animate-ping1 h-1 w-1 rounded-full', className)} />
+      <div className={cn('animate-ping2 h-1 w-1 rounded-full', className)} />
+      <div className={cn('animate-ping3 h-1 w-1 rounded-full', className)} />
     </div>
   );
 }
