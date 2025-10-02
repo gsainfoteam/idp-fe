31ea6c5d70f253a79e53e471ddd508b601851a5d
Hyunseung Ryu <rhseungg@gmail.com>
2025-10-03 04:41:35 +0900

    fix: order randomly change every fetch

--- src/features/client/hooks/use-client-list.ts
+++ src/features/client/hooks/use-client-list.ts
@@ -3,5 +3,10 @@ import { $api } from '@/features/core';
 export const useClientList = () => {
   const { data, isLoading, error, refetch } = $api.useQuery('get', '/client');
 
-  return { clients: data, isLoading, error, refetch };
+  return {
+    clients: data?.sort((a, b) => (a.name < b.name ? -1 : 1)),
+    isLoading,
+    error,
+    refetch,
+  };
 };
